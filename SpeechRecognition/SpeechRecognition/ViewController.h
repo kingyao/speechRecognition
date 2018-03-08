e717e1e5d720e3b5a6fc3ca97fe5b1c318029eec
KingYao <wangyao566@163.com>
2018-03-08 22:41:10 +0800

    创建工程

---
+++ SpeechRecognition/SpeechRecognition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpeechRecognition
+//
+//  Created by 王耀 on 2018/3/8.
+//  Copyright © 2018年 kingyao.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SpeechRecognition/SpeechRecognition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SpeechRecognition
+//
+//  Created by 王耀 on 2018/3/8.
+//  Copyright © 2018年 kingyao.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
